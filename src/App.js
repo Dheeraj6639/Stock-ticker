e0b32d12c4182150a7ec6fc55fe587c47a326cdb
Dheeraj6639 <dk0674200@gmail.com>
2024-06-06 11:05:30 +0530

    Stocks

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CrudOperations from './CRUD/crudOperations';
+
+function App() {
+  return (
+    <div className="App">
+      <CrudOperations/>
+    </div>
+  );
+}
+
+export default App;
